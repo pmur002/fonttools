c641ac65d16ea23bde2be4c0879eab0fb80715d4
Paul Murrell <paul@stat.auckland.ac.nz>
2023-06-12 13:03:19 +1200

    change start up to just attempt delay_load=TRUE import() of fontTools library (no attempt to install anything!) - first attempt to use ttLib will possibly prompt install of Python - will need documentation to suggest calling installFontTools() to resolve errors;  change initFontTools() to installFontTools() - calling this makes sure that we have Python and fontTools library.

--- R/zzz.R
+++ R/zzz.R
@@ -7,17 +7,20 @@ ttLib <- function() {
     tt <- get("ttLib", env=fontToolsEnv)
     if (is.null(tt))
         stop(paste("FontTools is not available;",
-                   "have you run initFontTools()?"))
+                   "try running installFontTools()?"))
     tt
 }
 
-initFontTools <- function() {
+installFontTools <- function() {
     ## Check whether Python exists and is bound to reticulate
+    ## Will offer to install Python (miniconda) otherwise
     havePython <- py_available(initialize=TRUE)
     if (!havePython) {
-        install_miniconda()
-        py_config()
+        stop(paste("Python must be installed to use",
+                   "the fonttools package"))
     }
+    ## Check whether Python library fontTools is installed
+    ## Will install fontTools if necessary
     haveFontTools <- py_module_available("fontTools")
     if (!haveFontTools) {
         py_install("fontTools")
@@ -32,23 +35,8 @@ initFontTools <- function() {
 }
 
 .onLoad <- function(libname, pkgname) {
-    ## May offer to install (miniconda) Python if no Python is available
-    ## Turn off the auto-install with ...
-    ## RETICULATE_MINICONDA_ENABLED=FALSE
-    havePython <- py_available(initialize=TRUE)
-    if (havePython) {
-        haveFontTools <- py_module_available("fontTools")
-        if (haveFontTools) {
-            assign("ttLib",
-                   import("fontTools.ttLib", delay_load = TRUE),
-                   env=fontToolsEnv)
-        } else {
-            warning(paste("Python fontTools library must be installed;",
-                          "see ?initFontTools"))
-        }
-    } else {
-        warning(paste("Python must be installed;",
-                      "see ?initFontTools"))
-    }
+    assign("ttLib",
+           import("fontTools", delay_load = TRUE),
+           env=fontToolsEnv)
 }
 
